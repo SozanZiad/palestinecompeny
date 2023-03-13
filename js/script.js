779867ac622e864e919527207e24900875d85792
SozanZiad <92858341+SozanZiad@users.noreply.github.com>
2023-03-13 18:49:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector('.search');
+var menu = document.querySelector('.search-div');
+
+
+    btn.addEventListener('click', function (){
+        menu.style.display =
+        menu.style.display === 'none' ? 'block' : 'none';
+    })
+
+ var add= document.querySelector('.add-circle').onclick =() =>
+ document.querySelector('.oevrllay').style.display='block'
+
+ var add= document.querySelector('.cancl img').onclick =() =>
+ document.querySelector('.oevrllay').style.display='none'
